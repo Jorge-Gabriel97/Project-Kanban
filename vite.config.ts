d41e4ec9833abe3e0e17f918520dba4441ca046c
Jorge Gabriel <jg3043505@gmail.com>
2026-03-19 16:56:17 -0300

    Fix:BugRunBild

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// O seu pode ter outras coisas, o importante é adicionar esse bloco "test"
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom', // <--- Liga o simulador de tela do navegador!
+    environment: 'jsdom',
   }
 })
